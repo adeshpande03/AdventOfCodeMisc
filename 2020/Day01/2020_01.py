0fa8f20d0a1e28083d7a488124dfb0ddbed4bd46
Akhil Deshpande <deshpande.akhil.s@gmail.com>
2024-12-01 22:17:07 -0600

    template changes

--- 2020/Day01/2020_01.py
+++ 2020/Day01/2020_01.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from aocd import get_data, submit
+import aocd
 import subprocess
 def part1(filename="input.txt"):
     with Path(__file__).with_name(filename).open("r") as f:
@@ -27,17 +27,16 @@ def part2(filename="input.txt"):
     return ans
 
 if __name__ == "__main__":
-    day = 5
-    year = 2022
+    day, year = aocd.get_day_and_year()
     with Path(__file__).with_name('input.txt').open("w") as inputData:
-        inputData.write(get_data(day=day, year=year))
+        inputData.write(aocd.get_data(day=day, year=year))
     result = subprocess.run(["aocd", f"{year}", f"{day}", "--example"], capture_output=True, text=True)
     lines = result.stdout.splitlines()  
     trimmed_lines = lines[3:-6] 
     with Path(__file__).with_name('sample.txt').open("w") as sampleData:
         sampleData.write("\n".join(trimmed_lines))
-    # print(p1ans:=part1())
-    # print(part2("sample.txt"))
-    # print(p2ans:=part2())
-    # submit(p2ans)
+    print(p1ans:=part1())
+    print(part2("sample.txt"))
+    print(p2ans:=part2())
+    aocd.submit(p2ans)
     
